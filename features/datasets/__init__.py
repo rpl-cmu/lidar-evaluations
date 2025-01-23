315b356168226e8ba1413bf1c98c32b0a1615050
Easton Potokar <contagon6@gmail.com>
2025-01-22 20:17:17 -0500

    Switched to separate python packages

--- features/datasets/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import Dataset
-from .enwide import EnWide
-from .newer_college_2020 import NewerCollege2020
-from .newer_college_2021 import NewerCollege2021
-
-__all__ = ["Dataset", "NewerCollege2020", "NewerCollege2021", "EnWide"]
