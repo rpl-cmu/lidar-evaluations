de26752d9edd231984041bd4a26dd0d7ed792350
Easton Potokar <contagon6@gmail.com>
2025-02-12 21:22:52 -0500

    Got imu integration working better

--- src/generate_imu_bias.py
+++ src/generate_imu_bias.py
@@ -1,8 +1,18 @@
 from typing import overload
-from evalio.types import ImuMeasurement, LidarMeasurement, SE3, SO3, ImuParams
+from evalio.datasets.base import Dataset
+from evalio.types import (
+    ImuMeasurement,
+    LidarMeasurement,
+    SE3,
+    SO3,
+    ImuParams,
+    Stamp,
+    Trajectory,
+)
 from evalio.cli import DatasetBuilder
 import numpy as np
 from dataclasses import dataclass
+from copy import deepcopy
 
 import gtsam
 from gtsam import (
@@ -19,6 +29,34 @@ from gtsam import (
 from gtsam.imuBias import ConstantBias  # type: ignore
 from gtsam.symbol_shorthand import X, V, B  # type: ignore
 from tqdm import tqdm
+
+
+@dataclass(kw_only=True)
+class OptimizationResult:
+    stamps: list[Stamp]
+    poses: list[Pose3]
+    vel: np.ndarray
+    gyro_bias: np.ndarray
+    accel_bias: np.ndarray
+
+
+@dataclass
+class NavState:
+    rot: Rot3
+    trans: np.ndarray
+    velocity: np.ndarray
+
+    # gtsam doesn't expose this function... sigh
+    def update(
+        self, accel: np.ndarray, gyro: np.ndarray, gravity: np.ndarray, dt: float
+    ) -> None:
+        rot = self.rot * Rot3.Expmap(gyro * dt)
+        vel = self.velocity + (self.rot.rotate(accel) + gravity) * dt
+        trans = self.trans + vel * dt + 0.5 * (self.rot.rotate(accel) + gravity) * dt**2
+
+        self.rot = rot
+        self.velocity = vel
+        self.trans = trans
 
 
 @overload
@@ -49,139 +87,227 @@ def convert(value: Pose3 | SE3) -> Pose3 | SE3:
         return PreintegratedCombinedMeasurements(pim_params)
 
 
-# Create a dataset
-# double checked
-name = "hilti_2022/construction_upper_level_1"
-# name = "newer_college_2021/quad-easy"
-# name = "newer_college_2020/01_short_experiment"
-# TODO: This one looks bad
-# name = "oxford_spires/keble_college_02"
+# returns imu measurements, lidar stamps, and ground truth
+def load_data(dataset: Dataset) -> tuple[list[ImuMeasurement], list[Stamp], Trajectory]:
+    # Get ground truth
+    gt = dataset.ground_truth()
 
-# def estimate_biases(name: str):
-dataset = DatasetBuilder.parse(name)[0].build()
+    # Gather all imu data
+    imu_data: list[ImuMeasurement] = []
+    lidar_stamps = []
+    for mm in tqdm(dataset):
+        if isinstance(mm, ImuMeasurement):
+            imu_data.append(mm)
+        elif isinstance(mm, LidarMeasurement):
+            lidar_stamps.append(mm.stamp)
 
-# Get ground truth
-gt = dataset.ground_truth()
-
-# Gather all imu data
-imu_data: list[ImuMeasurement] = []
-# lidar_stamps = []
-for mm in tqdm(dataset):
-    if isinstance(mm, ImuMeasurement):
-        imu_data.append(mm)
-    # elif isinstance(mm, LidarMeasurement):
-    #     lidar_stamps.append(mm.stamp)
+    return imu_data, lidar_stamps, gt
 
 
-# Initialize orientation using the first imu measurements
-num_used = 100
-a = np.array([i.accel for i in imu_data[:num_used]])
-w = np.array([i.gyro for i in imu_data[:num_used]])
-ez_W = np.array([0, 0, 1])
-e_acc = np.mean(a, axis=0)
-e_acc /= np.linalg.norm(e_acc)
-angle = np.arccos(ez_W @ e_acc)
-poseDelta = np.zeros(6)
-poseDelta[:3] = np.cross(ez_W, e_acc)
-poseDelta[:3] *= angle / np.linalg.norm(poseDelta[:3])
-imu0_T_x0 = Pose3.Expmap(-poseDelta)
+def estimate_biases(
+    imu_data: list[ImuMeasurement], imu_params: ImuParams, gt: Trajectory
+) -> OptimizationResult:
+    # Initialize orientation using the first imu measurements
+    num_used = 100
+    a = np.array([i.accel for i in imu_data[:num_used]])
+    w = np.array([i.gyro for i in imu_data[:num_used]])
+    ez_W = np.array([0, 0, 1])
+    e_acc = np.mean(a, axis=0)
+    e_acc /= np.linalg.norm(e_acc)
+    angle = np.arccos(ez_W @ e_acc)
+    poseDelta = np.zeros(6)
+    poseDelta[:3] = np.cross(ez_W, e_acc)
+    poseDelta[:3] *= angle / np.linalg.norm(poseDelta[:3])
+    imu0_T_x0 = Pose3.Expmap(-poseDelta)
 
-# bias init estimate
-avg_w = np.mean(w, axis=0)
-vio_R_x0 = imu0_T_x0.rotation().matrix()
-avg_a = np.mean(a, axis=0)
-# Remove gravity from init frame, then back to body frame
-avg_a = vio_R_x0.T @ (vio_R_x0 @ avg_a + dataset.imu_params().gravity)
-bias0 = ConstantBias(avg_a, avg_w)
+    # bias init estimate
+    avg_w = np.mean(w, axis=0)
+    vio_R_x0 = imu0_T_x0.rotation().matrix()
+    avg_a = np.mean(a, axis=0)
+    # Remove gravity from init frame, then back to body frame
+    avg_a = vio_R_x0.T @ (vio_R_x0 @ avg_a + imu_params.gravity)
+    bias0 = ConstantBias(avg_a, avg_w)
 
-# Line up both poses
-imu_idx = 0
-gt_idx = 0
-while gt.stamps[gt_idx] < imu_data[imu_idx].stamp:
-    gt_idx += 1
-while imu_data[imu_idx].stamp < gt.stamps[gt_idx]:
-    imu_idx += 1
-
-imu_data = imu_data[imu_idx:]
-gt.stamps = gt.stamps[gt_idx:]
-gt.poses = gt.poses[gt_idx:]
-
-# Align ground truth to imu init
-imu0_T_x0 = convert(imu0_T_x0)
-imu0_T_gt0 = imu0_T_x0 * gt.poses[0].inverse()
-gt.poses = [imu0_T_gt0 * p for p in gt.poses]
-
-
-# Create factor graph!
-graph = NonlinearFactorGraph()
-values = Values()
-pim = convert(dataset.imu_params())
-pose_noise = gtsam.noiseModel.Isotropic.Sigma(6, 1e-4)  # type: ignore
-vel_noise = gtsam.noiseModel.Isotropic.Sigma(3, 1e-4)  # type: ignore
-bias_noise = gtsam.noiseModel.Isotropic.Sigma(6, 1e-2)  # type: ignore
-
-pose = convert(gt.poses[0])
-values.insert(X(0), pose)
-values.insert(V(0), np.zeros(3))
-values.insert(B(0), bias0)
-
-graph.addPriorPose3(X(0), pose, pose_noise)
-graph.addPriorVector(V(0), np.zeros(3), vel_noise)
-graph.addPriorConstantBias(B(0), bias0, bias_noise)
-
-imu_idx = 0
-gt_idx = 1
-done = False
-while not done:
-    if gt_idx >= len(gt.stamps) or imu_idx >= len(imu_data):
-        done = True
-        continue
-
-    if imu_data[imu_idx].stamp < gt.stamps[gt_idx]:
-        dt = imu_data[imu_idx + 1].stamp - imu_data[imu_idx].stamp
-        pim.integrateMeasurement(imu_data[imu_idx].accel, imu_data[imu_idx].gyro, dt)
-        imu_idx += 1
-    else:
-        # Add all value estimates
-        values.insert(X(gt_idx), pose)
-        values.insert(V(gt_idx), np.zeros(3))
-        values.insert(B(gt_idx), bias0)
-        # Add prior factor
-        pose = convert(gt.poses[gt_idx])
-        graph.addPriorPose3(X(gt_idx), pose, pose_noise)
-        # Add imu factor
-        imu_factor = CombinedImuFactor(
-            X(gt_idx - 1),
-            V(gt_idx - 1),
-            X(gt_idx),
-            V(gt_idx),
-            B(gt_idx - 1),
-            B(gt_idx),
-            pim,
-        )
-        graph.push_back(imu_factor)
-        # reset things
+    # Line up both poses
+    imu_idx = 0
+    gt_idx = 0
+    while gt.stamps[gt_idx] < imu_data[imu_idx].stamp:
         gt_idx += 1
-        pim.resetIntegrationAndSetBias(bias0)
+    while imu_data[imu_idx].stamp < gt.stamps[gt_idx]:
+        imu_idx += 1
 
-# Optimize!
-params = LevenbergMarquardtParams()
-optimizer = LevenbergMarquardtOptimizer(graph, values, params)
-result = optimizer.optimize()
+    imu_data = imu_data[imu_idx:]
+    gt.stamps = gt.stamps[gt_idx:]
+    gt.poses = gt.poses[gt_idx:]
+
+    # Align ground truth to imu init
+    imu0_T_x0 = convert(imu0_T_x0)
+    imu0_T_gt0 = imu0_T_x0 * gt.poses[0].inverse()
+    gt.poses = [imu0_T_gt0 * p for p in gt.poses]
+
+    # Create factor graph!
+    graph = NonlinearFactorGraph()
+    values = Values()
+    pim = convert(imu_params)
+    pose_noise = gtsam.noiseModel.Isotropic.Sigma(6, 1e-6)  # type: ignore
+    vel_noise = gtsam.noiseModel.Isotropic.Sigma(3, 1e-6)  # type: ignore
+    bias_noise = gtsam.noiseModel.Isotropic.Sigma(6, 1)  # type: ignore
+
+    pose = convert(gt.poses[0])
+    values.insert(X(0), pose)
+    values.insert(V(0), np.zeros(3))
+    values.insert(B(0), bias0)
+
+    graph.addPriorPose3(X(0), pose, pose_noise)
+    graph.addPriorVector(V(0), np.zeros(3), vel_noise)
+    graph.addPriorConstantBias(B(0), bias0, bias_noise)
+
+    imu_idx = 0
+    gt_idx = 1
+    done = False
+    while not done:
+        if gt_idx >= len(gt.stamps) or imu_idx >= len(imu_data):
+            done = True
+            continue
+
+        if imu_data[imu_idx].stamp < gt.stamps[gt_idx]:
+            dt = imu_data[imu_idx + 1].stamp - imu_data[imu_idx].stamp
+            pim.integrateMeasurement(
+                imu_data[imu_idx].accel, imu_data[imu_idx].gyro, dt
+            )
+            imu_idx += 1
+        else:
+            # Add all value estimates
+            values.insert(X(gt_idx), pose)
+            values.insert(V(gt_idx), np.zeros(3))
+            values.insert(B(gt_idx), bias0)
+            # Add prior factor
+            pose = convert(gt.poses[gt_idx])
+            graph.addPriorPose3(X(gt_idx), pose, pose_noise)
+            # Add imu factor
+            imu_factor = CombinedImuFactor(
+                X(gt_idx - 1),
+                V(gt_idx - 1),
+                X(gt_idx),
+                V(gt_idx),
+                B(gt_idx - 1),
+                B(gt_idx),
+                pim,
+            )
+            graph.push_back(imu_factor)
+            # reset things
+            gt_idx += 1
+            pim.resetIntegrationAndSetBias(bias0)
+
+    # Optimize!
+    params = LevenbergMarquardtParams()
+    optimizer = LevenbergMarquardtOptimizer(graph, values, params)
+    result = optimizer.optimize()
+
+    biases = np.array(
+        [result.atConstantBias(B(i)).vector() for i in range(len(gt.stamps))]
+    )
+    gyro = biases[:, 3:]
+    accel = biases[:, :3]
+    poses = [result.atPose3(X(i)) for i in range(len(gt.stamps))]
+    vel = np.array([result.atVector(V(i)) for i in range(len(gt.stamps))])
+
+    max_norm = np.max(np.linalg.norm(biases[:, :3], axis=1))
+    assert max_norm < 0.5, f"Max accel bias norm is too large: {max_norm}"
+
+    import matplotlib.pyplot as plt
+    from wrappers import plt_show
+
+    fig, ax = plt.subplots(3, 2, layout="constrained")
+    for i in range(3):
+        ax[i, 0].plot(gyro[:, i])
+        ax[i, 1].plot(accel[:, i])
+    plt_show("figures/biases.png")
+
+    return OptimizationResult(
+        stamps=gt.stamps, poses=poses, vel=vel, gyro_bias=gyro, accel_bias=accel
+    )
 
 
-# plot the biases to double check they seem reasonable
-# import matplotlib.pyplot as plt
-# from wrappers import plt_show
+def integrate(
+    imu_data: list[ImuMeasurement],
+    opt_result: OptimizationResult,
+    gravity: np.ndarray,
+) -> tuple[list[Stamp], list[NavState]]:
+    """
+    TODO:
 
-# biases = np.array([result.atConstantBias(B(i)).vector() for i in range(len(gt.stamps))])
+    Need to do two things here:
+    1) Integrate IMU data through a scan to use as initialization
+    2) Integrate IMU data at each IMU timestamp to get bias-corrected poses
 
-# max_norm = np.max(np.linalg.norm(biases[:, :3], axis=1))
-# print(result.atConstantBias(B(0)), result.atConstantBias(B(1000)))
-# print("max accel bias norm: ", max_norm)
+    Alternatively I could just integrate all the IMU measurements and save those poses
+    I can post process later to handle things
 
-# fig, ax = plt.subplots(2, 3, layout="constrained")
-# for i in range(3):
-#     ax[0, i].plot(biases[:, i])
-#     ax[1, i].plot(biases[:, i + 3])
-# plt_show("figures/bias.png")
+    Need initial pose to kick things off
+    """
+    state = NavState(
+        rot=opt_result.poses[0].rotation(),
+        velocity=opt_result.vel[0],
+        trans=opt_result.poses[0].translation(),
+    )
+    bias_idx = 0
+    stamps = []
+    results = []
+
+    for i in range(1, len(imu_data)):
+        imu = imu_data[i]
+        dt = imu.stamp - imu_data[i - 1].stamp
+
+        # Find the bias index
+        while (
+            bias_idx < len(opt_result.stamps) - 1
+            and imu.stamp > opt_result.stamps[bias_idx]
+        ):
+            bias_idx += 1
+
+        # unbias the imu data
+        accel = imu.accel - opt_result.accel_bias[bias_idx]
+        gyro = imu.gyro - opt_result.gyro_bias[bias_idx]
+
+        # Update and save
+        state.update(accel, gyro, gravity, dt)
+        results.append(deepcopy(state))
+        stamps.append(imu.stamp)
+
+    return stamps, results
+
+
+if __name__ == "__main__":
+    # Create a dataset
+    # double checked
+    name = "hilti_2022/construction_upper_level_1"
+    # name = "newer_college_2021/quad-easy"
+    # name = "newer_college_2020/01_short_experiment"
+    # TODO: This one looks bad
+    # name = "oxford_spires/keble_college_02"
+    dataset = DatasetBuilder.parse(name)[0].build()
+
+    imu_data, lidar_stamps, gt = load_data(dataset)
+    # Try doing a smaller subset to make sure not moving at start
+    # gt.poses = gt.poses[:20]
+    # gt.stamps = gt.stamps[:20]
+    opt_result = estimate_biases(imu_data, dataset.imu_params(), gt)
+    stamps, results = integrate(imu_data, opt_result, dataset.imu_params().gravity)
+
+    # # plot the poses to double check they seem reasonable
+    # import matplotlib.pyplot as plt
+    # from wrappers import plt_show
+
+    # fig, ax = plt.subplots(1, 1, layout="constrained")
+    # num = 700
+    # x = np.asarray([p.translation()[0] for p in opt_result.poses])[:num]
+    # y = np.asarray([p.translation()[1] for p in opt_result.poses])[:num]
+    # ax.plot(x, y, label="Ground Truth")
+
+    # x = np.asarray([p.trans[0] for p in results])[: num * 40]
+    # y = np.asarray([p.trans[1] for p in results])[: num * 40]
+    # # print(results[:10])
+    # ax.plot(x, y, label="Integrated")
+    # ax.legend()
+    # plt_show("figures/integrated_poses.png")
