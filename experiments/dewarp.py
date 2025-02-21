12ec2cf2186a8e449f299f6a96bfbab637d48fd7
Easton Potokar <contagon6@gmail.com>
2025-02-20 20:54:22 -0500

    Clean up dewarp and init figure

--- experiments/dewarp.py
+++ experiments/dewarp.py
@@ -3,14 +3,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import polars as pl
 import sys
-import numpy as np
 
 sys.path.append("src")
 from params import ExperimentParams, Feature, Initialization, Dewarp
 from wrappers import parser, plt_show, setup_plot
 from run import run_multithreaded
 from stats import compute_cache_stats
-from env import ALL_TRAJ, LEN, RESULTS_DIR
+from env import ALL_TRAJ, LEN, RESULTS_DIR, COL_WIDTH
 
 
 dir = RESULTS_DIR / "25.02.18_dewarp_with_edges_cv_init"
@@ -22,7 +21,6 @@ def run(num_threads: int):
         Dewarp.Identity,
         Dewarp.ConstantVelocity,
         Dewarp.Imu,
-        # Dewarp.GroundTruthConstantVelocity,
     ]
 
     # ------------------------- Compute product of options ------------------------- #
@@ -43,8 +41,6 @@ def run(num_threads: int):
 def plot(name: str, force: bool):
     df = compute_cache_stats(dir, force=force)
 
-    # df = df.filter(pl.col("Initialization") == "GroundTruth")
-
     # Get identity versions
     df_identity = df.filter(pl.col("Dewarp") == "None").select(["dataset", "w100_RTEt"])
 
@@ -55,13 +51,18 @@ def plot(name: str, force: bool):
         df = df.with_columns(
             percent=pl.when(pl.col("dataset") == dataset)
             # THIS IS THE LINE THAT ACTUALLY COMPUTES THINGS
-            .then(pl.col("w100_RTEt") - id_val)
+            .then(100 * (pl.col("w100_RTEt") - id_val) / id_val)
             .otherwise(pl.col("percent"))
         )
 
     _c = setup_plot()
     fig, ax = plt.subplots(
-        1, 1, figsize=(5, 3), layout="constrained", sharey=False, sharex=True
+        1,
+        1,
+        figsize=(COL_WIDTH + 0.5, 2.25),
+        layout="constrained",
+        sharey=False,
+        sharex=True,
     )
 
     sns.lineplot(
@@ -77,31 +78,38 @@ def plot(name: str, force: bool):
         legend=True,
     )
     # blank line for the legend
-    ax.plot(np.NaN, np.NaN, "-", color="none", label=" ")
+    # ax.plot(np.NaN, np.NaN, "-", color="none", label=" ")
 
     # Reorder the legend to put blank one in the right spot
     handles, labels = ax.get_legend_handles_labels()
-    handles.insert(5, handles.pop(-1))
-    labels.insert(5, labels.pop(-1))
+    # handles.insert(5, handles.pop(-1))
+    # labels.insert(5, labels.pop(-1))
+    handles = handles[-3:]
+    labels = labels[-3:]
 
     ax.legend().set_visible(False)
-    ax.tick_params(axis="x", pad=-1, rotation=90)
-    ax.tick_params(axis="y", pad=-1)
+    ax.tick_params(axis="x", pad=-3, rotation=90, labelsize=7)
+    ax.tick_params(axis="y", pad=-2.4)
 
-    ax.set_ylabel("Difference from None $(m)$", labelpad=3)
+    ax.set_ylabel(r"$\text{RTEt}_{10}$ Change (%)", labelpad=3)
 
     traj = df.select("Trajectory").unique().to_numpy()
     xmax = len(traj) - 1
     extra = 1.0
     ax.set_xlim(0.0 - extra, xmax + extra)
 
-    fig.legend(
+    leg = fig.legend(
         handles=handles,
         labels=labels,
         ncol=3,
-        loc="outside lower center",
+        borderpad=0.2,
         labelspacing=0.15,
-    )
+        loc="outside upper left",
+        columnspacing=4.0,
+        bbox_to_anchor=(0.105, 1.12),
+    ).get_frame()
+    leg.set_boxstyle("square")  # type: ignore
+    leg.set_linewidth(1.0)
     plt_show(name)
 
 
--- experiments/init.py
+++ experiments/init.py
@@ -2,10 +2,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from itertools import product
 import sys
-import numpy as np
 
 sys.path.append("src")
-from env import ALL_TRAJ, LEN, RESULTS_DIR
+from env import ALL_TRAJ, COL_WIDTH, LEN, RESULTS_DIR
 from params import ExperimentParams, Feature, Initialization
 from stats import compute_cache_stats
 from run_init import run_multithreaded
@@ -39,7 +38,12 @@ def plot(name: str, force: bool):
 
     _c = setup_plot()
     fig, ax = plt.subplots(
-        1, 1, figsize=(5, 3), layout="constrained", sharey=False, sharex=True
+        1,
+        1,
+        figsize=(COL_WIDTH + 0.5, 2.0),
+        layout="constrained",
+        sharey=False,
+        sharex=True,
     )
 
     sns.lineplot(
@@ -55,31 +59,38 @@ def plot(name: str, force: bool):
         legend=True,
     )
     # blank line for the legend
-    ax.plot(np.NaN, np.NaN, "-", color="none", label=" ")
+    # ax.plot(np.NaN, np.NaN, "-", color="none", label=" ")
 
     # Reorder the legend to put blank one in the right spot
     handles, labels = ax.get_legend_handles_labels()
-    handles.insert(5, handles.pop(-1))
-    labels.insert(5, labels.pop(-1))
+    # handles.insert(5, handles.pop(-1))
+    # labels.insert(5, labels.pop(-1))
+    handles = handles[-3:]
+    labels = labels[-3:]
 
     ax.legend().set_visible(False)
-    ax.tick_params(axis="x", pad=-1, rotation=90)
-    ax.tick_params(axis="y", pad=-1)
+    ax.tick_params(axis="x", pad=-3, rotation=90, labelsize=7)
+    ax.tick_params(axis="y", pad=-2.4)
 
     traj = df.select("Trajectory").unique().to_numpy()
     xmax = len(traj) - 1
     extra = 1.0
     ax.set_xlim(0.0 - extra, xmax + extra)
 
-    ax.set_ylabel(r"$RTEt_{10}\ (m)$", labelpad=1)
+    ax.set_ylabel(r"$\text{RTEt}_{10}\ \ (m)$", labelpad=-2)
     ax.set_yscale("log")
-    fig.legend(
+    leg = fig.legend(
         handles=handles,
         labels=labels,
         ncol=3,
-        loc="outside lower center",
+        borderpad=0.2,
         labelspacing=0.15,
-    )
+        loc="outside upper left",
+        columnspacing=3.75,
+        bbox_to_anchor=(0.09, 1.14),
+    ).get_frame()
+    leg.set_boxstyle("square")  # type: ignore
+    leg.set_linewidth(1.0)
     plt_show(name)
 
 
