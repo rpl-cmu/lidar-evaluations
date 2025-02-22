5689bbfab4dad21d2249aa308bb76d54562b2004
Easton Potokar <contagon6@gmail.com>
2025-02-21 19:47:24 -0500

    Plane to plane coming along

--- experiments/plane_plane.py
+++ experiments/plane_plane.py
@@ -2,7 +2,6 @@ from itertools import product
 import seaborn as sns
 import matplotlib.pyplot as plt
 import sys
-import numpy as np
 import polars as pl
 
 sys.path.append("src")
@@ -10,32 +9,27 @@ from params import ExperimentParams, Feature, Initialization
 from wrappers import parser, plt_show, setup_plot
 from run import run_multithreaded
 from stats import compute_cache_stats
-from env import ALL_TRAJ, LEN, RESULTS_DIR, SUBSET_TRAJ
+from env import ALL_TRAJ, LEN, RESULTS_DIR, COL_WIDTH
 
 # ------------------------- Everything to sweep over ------------------------- #
+# TODO: Rerun with ground truth
+# dir = RESULTS_DIR / "25.02.20_plane_plane"
 dir = RESULTS_DIR / "25.02.18_plane_plane_cv"
 
 
 def run(num_threads: int):
-    init = [
-        # Initialization.Identity,
-        Initialization.ConstantVelocity,
-        # Initialization.Imu,
-        # Initialization.GroundTruth,
-    ]
-
     use_plane_to_plane = [True, False]
 
     # ------------------------- Compute product of options ------------------------- #
     experiments = [
         ExperimentParams(
-            name=f"plane_plane_{pp}_{i.name}",
+            name=f"plane_plane_{pp}",
             dataset=d,
             features=[Feature.Planar],
             use_plane_to_plane=pp,
-            init=i,
+            init=Initialization.GroundTruth,
         )
-        for i, pp, d in product(init, use_plane_to_plane, ALL_TRAJ)
+        for pp, d in product(use_plane_to_plane, ALL_TRAJ)
     ]
 
     run_multithreaded(experiments, dir, num_threads=num_threads, length=LEN)
@@ -56,13 +50,18 @@ def plot(name: str, force: bool):
         df = df.with_columns(
             percent=pl.when(pl.col("dataset") == dataset)
             # THIS IS THE LINE THAT ACTUALLY COMPUTES THINGS
-            .then(pl.col("w100_RTEt") - id_val)
+            .then(100 * (pl.col("w100_RTEt") - id_val) / id_val)
             .otherwise(pl.col("percent"))
         )
 
     _c = setup_plot()
     fig, ax = plt.subplots(
-        1, 1, figsize=(5, 3), layout="constrained", sharey=False, sharex=True
+        1,
+        1,
+        figsize=(COL_WIDTH + 0.5, 2.0),
+        layout="constrained",
+        sharey=False,
+        sharex=True,
     )
 
     sns.lineplot(
@@ -73,37 +72,38 @@ def plot(name: str, force: bool):
         hue="Dataset",
         style="Planar Type",
         markers=["s", "X"],
-        style_order=["Plane-To-Plane", "Point-To-Plane"],
+        style_order=["Point-To-Plane", "Plane-To-Plane"],
         dashes=False,
         legend=True,
     )
-    # blank line for the legend
-    # ax.plot(np.NaN, np.NaN, "-", color="none", label=" ")
-
     # Reorder the legend to put blank one in the right spot
     handles, labels = ax.get_legend_handles_labels()
-    # handles.insert(5, handles.pop(-1))
-    # labels.insert(5, labels.pop(-1))
     handles = handles[-2:]
     labels = labels[-2:]
 
     ax.legend().set_visible(False)
-    ax.tick_params(axis="x", pad=-1, rotation=90)
-    ax.tick_params(axis="y", pad=-1)
+    ax.tick_params(axis="x", pad=-3, rotation=90, labelsize=7)
+    ax.tick_params(axis="y", pad=-2.4)
+
+    ax.set_ylabel(r"$\text{RTEt}_{10}$ Change (%)", labelpad=3)
 
     traj = df.select("Trajectory").unique().to_numpy()
     xmax = len(traj) - 1
     extra = 1.0
     ax.set_xlim(0.0 - extra, xmax + extra)
 
-    ax.set_ylabel(r"Difference from Point-To-Point $(m)$", labelpad=1)
-    fig.legend(
+    leg = fig.legend(
         handles=handles,
         labels=labels,
         ncol=3,
-        loc="outside lower center",
+        borderpad=0.2,
         labelspacing=0.15,
-    )
+        loc="outside upper left",
+        columnspacing=9.9,
+        bbox_to_anchor=(0.1055, 1.13),
+    ).get_frame()
+    leg.set_boxstyle("square")  # type: ignore
+    leg.set_linewidth(1.0)
     plt_show(name)
 
 
